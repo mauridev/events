7d629774ee78549f1832fa32ff361bafeb526bc5
Mauricio Arismendi <arismendimauricio@gmail.com>
2019-10-20 22:08:11 -0300

    add-events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,51 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EventListComponent } from './events/events-list.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { CreateEventComponent } from './events/create-event.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './errors/404.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+
+import { appRoutes } from './routes';
+
+import { EventListResolver } from './events/events-list-resolver.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EventListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    NavBarComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [EventService, ToastrService],
+  providers: [
+    EventService, 
+    ToastrService, 
+    EventRouteActivator,
+    EventListResolver,
+    { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState}
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty)
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  return true
+  
+}
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,31 +1,32 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event.service'
 import { ToastrService } from '../common/toastr.service'
-
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
-    selector: 'events-list',
     template: `
     <div>
         <h1>Próximos Eventos</h1>
         <hr />
         <div class="row">
             <div *ngFor="let event of events"  class="col-md-5">
-                <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
+                <event-thumbnail [routerLink]="['/events', event.id]" (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
             </div>
         </div>
     </div>
     `
 })
 export class EventListComponent implements OnInit {
-  events
+  events: any
 
-  constructor(private eventService: EventService, private toastr: ToastrService) {
+  constructor(private eventService: EventService,
+    private toastr: ToastrService,
+    private route:ActivatedRoute) {
        
   }
 
   ngOnInit() {
-    this.events = this.eventService.getEvents()
+    this.events = this.route.snapshot.data['events']
   }
 
   handleThumbnailClick(eventName) {
